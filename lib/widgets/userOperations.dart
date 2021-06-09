436e0ef6a2a452c76281d66c110574b8684b12dd
mickey8121 <57273457+mickey8121@users.noreply.github.com>
2021-06-09 19:47:38 +0300

    Update userOperations.dart

--- lib/widgets/userOperations.dart
+++ lib/widgets/userOperations.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'package:tinkoff_invest/tinkoff_invest.dart';
+// import 'package:tinkoff_invest/tinkoff_invest.dart';
 
 import 'package:advanced_tinkoff_invest/models/extensions/StringExtension.dart';
 
 import 'package:advanced_tinkoff_invest/utils/currencySymbols.dart';
 import 'package:advanced_tinkoff_invest/models/api.dart';
+
+const instrumentIcons = {
+  'Stock': Icons.bar_chart_rounded,
+  'Bond': Icons.featured_play_list_rounded,
+  'Etf': Icons.pie_chart_rounded,
+  'Currency': Icons.money,
+};
 
 class UserOperations extends StatefulWidget {
   @override
@@ -20,8 +27,6 @@ class _UserOperationsState extends State<UserOperations> {
   @override
   void initState() {
     super.initState();
-
-    setState(() => _loading = false);
     _loadData();
   }
 
@@ -45,6 +50,8 @@ class _UserOperationsState extends State<UserOperations> {
     final trades = operation['trades'];
     final title = '$type ';
     final instrument = operation['instrument'];
+    final instrumentName = instrument?['name'];
+    final icon = instrumentIcons[instrumentType];
 
     final paymentData = '$payment$currencySymbol';
     String operationDate = DateFormat('yMMMMd', 'en_US').format(DateTime.parse(operation['date']));
@@ -66,18 +73,19 @@ class _UserOperationsState extends State<UserOperations> {
                     Row(
                       children: [
                         Text(
-                          instrument?['name'] ?? '',
+                          instrument?['name'] != null ? '${instrument?['name']}' : type,
                           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)
                         ),
+                        ...(icon != null ? [Icon(icon)] : []),
                       ],
                     ),
-                    Text(type),
+                    SizedBox(height: 2),
+                    Text(instrument?['name'] != null ? type : ''),
                     SizedBox(height: 5),
-                    Text('$instrumentType'),
                     SizedBox(height: 5),
                     Text(operationDate),
-                    SizedBox(height: 5),
-                    Text('$figi'),
+                    // SizedBox(height: 5),
+                    // Text('$figi'),
                   ],
                 ),
                 Text(
@@ -104,7 +112,8 @@ class _UserOperationsState extends State<UserOperations> {
           ? Center(child: Text('Loading', style: Theme.of(context).textTheme.headline3))
           : Container(
             child: ListView(
-              children: _operations != null ? _operations!.map((o) => _operationCard(o)).toList() : [],
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+              children: _operations?.map((o) => _operationCard(o)).toList() ?? [],
             )
           )
        ),
