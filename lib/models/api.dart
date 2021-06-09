8556a03ef01741f31507a5fce5e0bae4b33ddb96
mickey8121 <57273457+mickey8121@users.noreply.github.com>
2021-06-09 17:36:32 +0300

    Rewrite API

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -1,13 +1,34 @@
+// import 'package:async/async.dart';
+import 'package:async/async.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tinkoff_invest/tinkoff_invest.dart';
 
-class Api extends ChangeNotifier {
-  var _api;
+import 'package:advanced_tinkoff_invest/services/LocalStorage.dart';
+import 'package:advanced_tinkoff_invest/utils/operationsHC.dart' as operationsHC;
+
+import 'package:advanced_tinkoff_invest/models/extensions/PortfolioExtension.dart';
+import 'package:advanced_tinkoff_invest/models/extensions/MarketInstrumentListExtension.dart';
+import 'package:advanced_tinkoff_invest/models/extensions/OperationExtension.dart';
+import 'package:advanced_tinkoff_invest/models/extensions/OperationsExtension.dart';
+
+class API extends ChangeNotifier {
+  API() {
+    _initLocalStorage();
+  }
+
+  void _initLocalStorage() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    this.localStorage = LocalStorage(prefs: sharedPreferences);
+  }
+
+  TinkoffInvestApi? _api;
   bool isAuthorized = false;
+  LocalStorage? localStorage;
 
   TinkoffInvestApi? get tinkoffApi => _api;
 
-  void setInitApi(token, [sandboxMode = false, debug = false]) {
+  void setInitApi(token, [sandboxMode = false, debug = false]) async {
     _api = TinkoffInvestApi(token, sandboxMode: sandboxMode, debug: debug);
 
     if (_api?.user != null) isAuthorized = true;
@@ -16,26 +37,47 @@ class Api extends ChangeNotifier {
   }
 
   dynamic _responseHandling(dynamic entity) {
-    if (entity.isValue) {
-      try {
-        return entity.asValue!.value.payload;
-      } catch (e) {
-        throw e as String;
+    if (entity is ValueResult) {
+      if (entity.isValue) {
+        try {
+          return entity.asValue.value.payload;
+        } catch (e) {
+          throw e as String;
+        }
+      } else if (entity.isError && entity.asError != null) {
+        if (entity.asError?.error.runtimeType == String)
+          throw entity.asError!.error;
+        // else if (entity.asError?.error.payload.message != null)
+        //   throw entity.asError.error.payload.message;
       }
-    } else if (entity.isError && entity.asError != null) {
-      if (entity.asError.error.runtimeType == String)
-        throw entity.asError?.error;
-      else if (entity.asError.error.payload?.message != null) {
-        throw entity.asError.error.payload.message;
-      }
-    }
+    } else return entity;
 
     throw 'Error';
   }
 
-  Future<Portfolio> getPortfolio() async {
-    final portfolio = await tinkoffApi?.portfolio.load();
-    return _responseHandling(portfolio);
+  // LocalStorage methods
+  Map<String, dynamic>? getDataFromLocalStorage(String key) => localStorage?.getData(key: key);
+  void setDataToLocalStorage(String key, Map data) => localStorage?.setData(key: key, data: data);
+  //
+
+  Future<Map<String, dynamic>?> getPortfolio() async {
+    // print((await tinkoffApi!.portfolio.load()).runtimeType != ErrorResult);
+    var portfolio = getDataFromLocalStorage('portfolio');
+
+    if (portfolio == null) {
+      final portfolioLoad = await tinkoffApi?.portfolio.load();
+
+      try {
+        final portfolioResponse = _responseHandling(portfolioLoad);
+        portfolio = (portfolioResponse as Portfolio).toJson();
+        
+        setDataToLocalStorage('portfolio', portfolio);
+      } catch (e) {
+        throw e;
+      }
+    }
+
+    return portfolio;
   }
   
   Future<UserAccounts> getAccounts() async {
@@ -48,24 +90,42 @@ class Api extends ChangeNotifier {
     return _responseHandling(orders);
   }
 
-  Future<MarketInstrumentList> getInstrumentsList(String instrumentType) async {
-    var instrumentsList;
+  Future<Map<String, dynamic>?> getInstrumentsList(String instrumentType) async {
+    final localStorageKey = '${instrumentType}List';
+    
+    var instrumentsList = getDataFromLocalStorage(localStorageKey);
 
-    switch (instrumentType) {
-      case 'stock':
-        instrumentsList = await tinkoffApi?.market.stocks();
-        break;
-      case 'bond':
-        instrumentsList = await tinkoffApi?.market.bonds();
-        break;
-      case 'etf':
-        instrumentsList = await tinkoffApi?.market.etfs();
-        break;
-      default:
-        throw 'Error: instrumentType is incorrect';
+    if (instrumentsList == null) {
+      var instrumentResponse;
+
+      switch (instrumentType) {
+        case 'stock':
+          instrumentResponse = await getStocks();
+          break;
+        case 'bond':
+          instrumentResponse = await getBonds();
+          break;
+        case 'etf':
+          instrumentResponse = await getEtfs();
+          break;
+        case 'currency':
+          instrumentResponse = await getCurrencies();
+          break;
+        default:
+          throw 'Error: instrumentType is incorrect';
+      }
+
+      try {
+        final response = _responseHandling(instrumentResponse);
+        instrumentsList = (response as MarketInstrumentList).toJson();
+
+        setDataToLocalStorage(localStorageKey, instrumentsList);
+      } catch (e) {
+        throw e;
+      }
     }
 
-    return _responseHandling(instrumentsList);
+    return instrumentsList;
   }
   
   Future<MarketInstrumentList> getStocks() async {
@@ -83,9 +143,69 @@ class Api extends ChangeNotifier {
     return _responseHandling(etfs);
   }
 
+  Future<MarketInstrumentList> getCurrencies() async {
+    final currencies = await tinkoffApi?.market.currencies();
+    return _responseHandling(currencies);
+  }
+
   Future<Orderbook> getOrderbook(String figi, int depth) async {
     final orderbook = await tinkoffApi?.market.orderbook(figi, depth);
     return _responseHandling(orderbook);
+  }
+
+  Future<Map<String, dynamic>?> getAllOperations({ bool? noCache }) async {
+    Map<String, dynamic>? operations = getDataFromLocalStorage('operationss');
+
+    if (operations == null) {
+      // for real response ------------------------------------
+      // final DateTime nowDate = DateTime.now();
+      // final duration = Duration(days: 10000);
+      // final from = nowDate.subtract(duration);
+      // var operationsLoad = await tinkoffApi?.operations.load(from, nowDate);
+
+      final instrumentsList = {
+        'Stock': await getInstrumentsList('stock'),
+        'Currency': await getInstrumentsList('currency'),
+        'Bond': await getInstrumentsList('bond'),
+        'Etf': await getInstrumentsList('etf'),
+      };
+
+      try {
+        // var operationsResponse = _responseHandling(operationsLoad); // for real response
+        var operationsResponse = { 'operations': operationsHC.operations };
+
+        // for real response
+        // var operationsList = List.from((operationsResponse as Operations).toJson()['operations'] ?? []);
+
+        var extendedOperations =
+          // await Future.wait(operationsList.map((o) async { // for real response
+          await Future.wait(List.from(operationsResponse['operations'] ?? []).map((o) async {
+            var operation = Map.from(o); // make mutable
+
+            final figi = operation['figi'];
+            final instrumentType = operation['instrumentType'];
+            
+            if (figi != null && instrumentType != null) {
+              final instruments = instrumentsList['$instrumentType'];
+
+              if (instruments?['instruments'] != null) {
+                operation['instrument'] =
+                  instruments?['instruments'].firstWhere((instr) => instr['figi'] == figi);
+              }
+            }
+
+            return operation;
+          }).toList());
+
+        operations = { 'operations': extendedOperations };
+
+        setDataToLocalStorage('operations', operations);
+      } catch (e) {
+        throw e;
+      }
+    }
+
+    return operations;
   }
 
   /* 
@@ -123,14 +243,19 @@ class Api extends ChangeNotifier {
   }
 
   dynamic subscribeToInstrumentInfo(String figi) async {
-    _api.streaming.instrumentInfo.subscribe(
+    _api!.streaming.instrumentInfo.subscribe(
       figi,
       (event) => print('InstrumentInfo: ${event.payload}'),
     );
   }
 
-  Future<Function> subscribeToCandle(String figi, StreamingCandleInterval interval, Function cb) async {
-    await _api.streaming.candle.subscribe(figi, interval, cb);
-    return () async => await _api.streaming.candle.unsubscribe(figi, interval, cb);
+  Future<Function> subscribeToCandle(String figi, StreamingCandleInterval interval, listener) async {
+    _api?.streaming.candle.subscribe(figi, interval, listener);
+    return () => _api?.streaming.candle.unsubscribe(figi, interval, listener);
+  }
+
+  Function subscribeToOrderbook(String figi, int depth, listener) {
+    _api!.streaming.orderbook.subscribe(figi, 10, listener);
+    return () => _api?.streaming.orderbook.unsubscribe(figi, depth, listener);
   }
 }
