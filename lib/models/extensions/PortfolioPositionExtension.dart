2b17f7452acfa3a30036a83b8cdfa8d990e3d5eb
mickey8121 <57273457+mickey8121@users.noreply.github.com>
2021-06-12 14:00:40 +0300

    Update PortfolioPositionExtension.dart

--- lib/models/extensions/PortfolioPositionExtension.dart
+++ lib/models/extensions/PortfolioPositionExtension.dart
@@ -3,26 +3,26 @@ import 'package:tinkoff_invest/tinkoff_invest.dart';
 extension PortfolioPositionExtension on PortfolioPosition {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> _portfolioPositionJson = {
-      'figi': '${this.figi}',
-      'ticker': '${this.ticker}',
-      'isin': '${this.isin}',
-      'instrumentType': '${this.instrumentType.name}',
-      'balance': '${this.balance}',
-      'blocked': '${this.blocked}',
+      'figi': this.figi,
+      'ticker': this.ticker,
+      'isin': this.isin,
+      'instrumentType': this.instrumentType.name,
+      'balance': this.balance,
+      'blocked': this.blocked,
       'expectedYield': {
-        'value': '${this.expectedYield?.value}',
-        'currency': '${this.expectedYield?.currency.name}',
+        'value': this.expectedYield?.value,
+        'currency': this.expectedYield?.currency.name,
       },
-      'lots': '${this.lots}',
+      'lots': this.lots,
       'averagePositionPrice': {
-        'value': '${this.averagePositionPrice?.value}',
-        'currency': '${this.averagePositionPrice?.currency.name}',
+        'value': this.averagePositionPrice?.value,
+        'currency': this.averagePositionPrice?.currency.name,
       },
       'averagePositionPriceNoNkd': {
-        'value': '${this.averagePositionPriceNoNkd?.value}',
-        'currency': '${this.averagePositionPriceNoNkd?.currency.name}',
+        'value': this.averagePositionPriceNoNkd?.value,
+        'currency': this.averagePositionPriceNoNkd?.currency.name,
       },
-      'name': '${this.name}',
+      'name': this.name,
     };
 
     return _portfolioPositionJson;
