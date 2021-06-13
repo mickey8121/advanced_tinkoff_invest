5fcfb6595932d26127749e723d7112f71dcac17b
mickey8121 <57273457+mickey8121@users.noreply.github.com>
2021-06-13 16:22:07 +0300

    Create instrumentIcons.dart

---
+++ lib/utils/instrumentIcons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const instrumentIcons = {
+  'Stock': Icons.bar_chart_rounded,
+  'Bond': Icons.featured_play_list_rounded,
+  'Etf': Icons.pie_chart_rounded,
+  'Currency': Icons.money,
+};
