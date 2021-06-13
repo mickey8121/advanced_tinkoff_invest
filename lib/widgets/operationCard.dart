9121b08406d8b2dda483823b2cd81040d5aeca4f
mickey8121 <57273457+mickey8121@users.noreply.github.com>
2021-06-13 18:15:57 +0300

    Create operationCard

---
+++ lib/widgets/operationCard.dart
@@ -0,0 +1,96 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+import 'package:advanced_tinkoff_invest/models/extensions/StringExtension.dart';
+import 'package:advanced_tinkoff_invest/utils/currencySymbols.dart';
+import 'package:advanced_tinkoff_invest/utils/instrumentIcons.dart';
+
+class OperationCard extends StatelessWidget {
+  OperationCard({ Key? key, required this.operation }) : super(key: key);
+
+  final Map operation;
+
+  @override
+  Widget build(BuildContext context) {
+    final type = '${operation['operationType']}'.splitPascalCase().capitalize();
+    final instrumentType = '${operation['instrumentType']}'.splitPascalCase().capitalize();
+    final currencySymbol = currencySymbols[operation['currency']];
+    final payment = operation['payment'];
+    final instrument = operation['instrument'];
+    final instrumentName = instrument?['name'];
+    final icon = instrumentIcons[instrumentType];
+
+    String operationDate = DateFormat('yMMMMd', 'en_US').format(DateTime.parse(operation['date']));
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            // dense: true,
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Icon(icon, size: 30),
+                Text(
+                  instrument['ticker'],
+                  style: TextStyle(
+                    color: Colors.cyan[200],
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            title: Text(
+              instrumentName,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 18),
+            ),
+            subtitle: Text(type),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  '${payment > 0 ? '+' : ''}$payment',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: (
+                      payment.isNegative
+                        ? Colors.red[300]
+                        : payment != 0
+                          ? Colors.lightGreen[300]
+                          : null
+                    ),
+                  ),
+                ),
+                Text(
+                  currencySymbol != null ? ' $currencySymbol' : '',
+                  style: TextStyle(fontSize: 16,)
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 0, bottom: 10, left: 16, right: 16),
+            child: Text(
+              operationDate,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white38,
+              ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
--- lib/widgets/userOperations.dart
+++ lib/widgets/userOperations.dart
@@ -1,15 +1,5 @@
+import 'package:advanced_tinkoff_invest/widgets/operationCard.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-import 'package:advanced_tinkoff_invest/models/extensions/StringExtension.dart';
-import 'package:advanced_tinkoff_invest/utils/currencySymbols.dart';
-
-const instrumentIcons = {
-  'Stock': Icons.bar_chart_rounded,
-  'Bond': Icons.featured_play_list_rounded,
-  'Etf': Icons.pie_chart_rounded,
-  'Currency': Icons.money,
-};
 
 class UserOperations extends StatefulWidget {
   final List operations;
@@ -29,92 +19,13 @@ class _UserOperationsState extends State<UserOperations> {
     super.initState();
   }
 
-  Widget _operationCard(Map operation) {
-    final type = '${operation['operationType']}'.splitPascalCase().capitalize();
-    final instrumentType = '${operation['instrumentType']}'.splitPascalCase().capitalize();
-    final currencySymbol = currencySymbols[operation['currency']];
-    final payment = operation['payment'];
-    // final figi = operation['figi'];
-    // final trades = operation['trades'];
-    // final title = '$type ';
-    final instrument = operation['instrument'];
-    final instrumentName = instrument?['name'];
-    final icon = instrumentIcons[instrumentType];
-
-    final paymentData = '$payment$currencySymbol';
-    String operationDate = DateFormat('yMMMMd', 'en_US').format(DateTime.parse(operation['date']));
-    
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Container(
-        padding: EdgeInsets.only(top: 15, bottom: 10, left: 16, right: 16),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 7,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            instrumentName != null ? instrumentName : type,
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)
-                          ),
-                          ...(icon != null ? [Icon(icon)] : []),
-                        ],
-                      ),
-
-                      SizedBox(height: 2),
-                      Text(instrumentName != null ? type : ''),
-                      SizedBox(height: 5),
-                      SizedBox(height: 5),
-                      Text(operationDate),
-                    ],
-                  ),
-                ),
-
-                Text(
-                  paymentData,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: payment.isNegative ? Colors.red[700] : Colors.green[700],
-                  ),
-                  textAlign: TextAlign.end,
-                ),
-                // Expanded(
-                //   flex: 3,
-                //   child: Text(
-                //     paymentData,
-                //     style: TextStyle(
-                //       fontWeight: FontWeight.bold,
-                //       fontSize: 18,
-                //       color: payment.isNegative ? Colors.red[700] : Colors.green[700],
-                //     ),
-                //     textAlign: TextAlign.end,
-                //   ),
-                // ),
-              ],
-            )
-          ],
-        ),
-      )
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
        child: Container(
          child: ListView(
           padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-            children: _operations?.map((o) => _operationCard(o)).toList() ?? [],
+            children: _operations?.map((o) => OperationCard(operation: o)).toList() ?? [],
           )
         )
     );
