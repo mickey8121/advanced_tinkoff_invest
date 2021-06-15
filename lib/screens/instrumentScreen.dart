f7f2c69c82a4737130010c86fb3bd3973c68741a
mickey8121 <57273457+mickey8121@users.noreply.github.com>
2021-06-16 00:39:45 +0300

    Update instrumentScreen.dart

--- lib/screens/instrumentScreen.dart
+++ lib/screens/instrumentScreen.dart
@@ -1,6 +1,18 @@
+// test trading_chart
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:advanced_tinkoff_invest/utils/getDurationByInterval.dart';
+import 'package:http/http.dart' as http; // temporary
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:tinkoff_invest/tinkoff_invest.dart';
+import 'package:trading_chart/depth_chart.dart';
+import 'package:trading_chart/entity/depth_entity.dart';
+import 'package:trading_chart/entity/k_line_entity.dart';
+import 'package:trading_chart/k_chart_widget.dart';
+import 'package:trading_chart/utils/data_util.dart';
 import 'package:yahoofin/yahoofin.dart';
 // ignore: implementation_imports
 import 'package:yahoofin/src/models/stockQuote.dart';
@@ -34,11 +46,9 @@ class InstrumentScreen extends StatefulWidget {
 class _InstrumentScreenState extends State<InstrumentScreen> {
   bool _loading = true;
   CandleResolution _selectedInterval = CandleResolution.oneMin;
-  Map<String, dynamic>? _orderbookData;
-  // Orderbook? _orderbook;
-  List<Candle>? _candles;
-  StockQuote? _instrumentData;
-  Function? _unsubscribe;
+  late  List<Candle> _candles;
+  List<KLineEntity>? _kLines = [];
+  List<DepthEntity> _bids = [], _asks = [];
 
   @override
   void initState() {
@@ -46,65 +56,47 @@ class _InstrumentScreenState extends State<InstrumentScreen> {
     _initData();
   }
 
-  @override
-  void dispose() async {
-    super.dispose();
-    if (_unsubscribe != null) await _unsubscribe!();
-  }
+  // @override
+  // void dispose() async {
+  //   super.dispose();
+  //   if (_unsubscribe != null) await _unsubscribe!();
+  // }
 
   void _initData() async {
-    final yfin = YahooFin();
+    // final yfin = YahooFin();
 
-    final figi = widget.instrument?['figi'];
-    final ticker = widget.instrument?['ticker'];
+    // final figi = widget.instrument?['figi'];
+    // final ticker = widget.instrument?['ticker'];
 
-    StockInfo instrumentInfo = yfin.getStockInfo(ticker: ticker);
-
-    try {
-      _instrumentData = await instrumentInfo.getStockData();
-    } catch (e) {
-      print(e);
-    }
-
+    await _loadAsksAndBids();
     await _loadCandles(_selectedInterval);
-
-    final unsubscribe = context.read<API>().subscribeToOrderbook(
-      figi,
-      10,
-      (data) => setState(
-        () {
-          _orderbookData = {
-            'event': data.event,
-            'time': data.time,
-            'payload': data.payload,
-          };
-        }
-      ),
-    );
 
     setState(() {
       _loading = false;
-      _unsubscribe = unsubscribe;
     });
   }
 
-  Future<void> _loadCandles(CandleResolution interval) async {
-    Duration duration = Duration(days: 1);
+  Future<void> _loadAsksAndBids() async {
+    try {
+      final orderbook = await context.read<API>().getOrderbook(widget.instrument?['figi'], 20);
+      final asks = orderbook.asks
+        .map((ask) => DepthEntity(ask.price, ask.quantity.toDouble()))
+        .toList();
+      final bids = orderbook.bids
+        .map((bid) => DepthEntity(bid.price, bid.quantity.toDouble()))
+        .toList();
+      
+      bids.sort((elA, elB) => (elB.vol - elA.vol).toInt());
+      asks.sort((elA, elB) => (elA.vol - elB.vol).toInt());
 
-    switch (interval) {
-      case CandleResolution.hour:
-        duration = Duration(days: 7);
-        break;
-      case CandleResolution.week:
-      case CandleResolution.day:
-        duration = Duration(days: 365);
-        break;
-      case CandleResolution.month:
-        duration = Duration(days: 365 * 5);
-        break;
-      default:
-        break;
+      setState(() { _bids = bids; _asks = asks; });
+    } catch (e) {
+      print('_loadOrderbook $e');
     }
+  }
+
+  Future<void> _loadCandles(CandleResolution interval) async {
+    final duration = durationByInterval(interval);
 
     final nowDate = DateTime.now();
     final fromDate = nowDate.subtract(duration);
@@ -118,7 +110,25 @@ class _InstrumentScreenState extends State<InstrumentScreen> {
       interval: interval,
     );
 
-    setState(() { _loading = false; _candles = candles.candles; });
+    List<KLineEntity> kLines = candles.candles.map(
+      (candle) {
+        return KLineEntity.fromCustom(
+          amount: Random().nextInt(300).toDouble(),
+          ratio: 0.5,
+          change: 1,
+          open: candle.o,
+          close: candle.c,
+          high: candle.h,
+          low: candle.l,
+          vol: candle.v.toDouble(),
+          time: candle.time.millisecondsSinceEpoch,
+        );
+      }
+    ).toList();
+
+    DataUtil.calculate(kLines);
+
+    setState(() { _loading = false; _candles = candles.candles; _kLines = kLines; });
   }
 
   void _onTap(interval) {
@@ -130,79 +140,85 @@ class _InstrumentScreenState extends State<InstrumentScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final double? currentPrice = _instrumentData?.currentPrice;
-    final double? fiftyDaysAverageChange = _instrumentData?.fiftyDayAverageChange;
-
-    final String? regularMarketChange =
-      _instrumentData?.regularMarketChangePercent?.toStringAsFixed(3);
+    final viewHeight = (
+      MediaQuery.of(context).size.height
+      - AppBar().preferredSize.height
+      - MediaQuery.of(context).padding.top
+      // - 50 // margin from the bottom edge
+    );
 
     return Container(
        child: Scaffold(
           appBar: AppBar(title: Text(widget.instrument?['name'])),
-          body: Column(
-            children: [
-              CandlesticksChart(candles: _candles as List, isLoading: _loading),
+          body: ListView(
+          children: <Widget>[
+            Stack(children: <Widget>[
               Container(
-                height: 60,
-                padding: EdgeInsets.only(top: 10),
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    SizedBox(width: 10),
-                    Wrap(
-                      spacing: 10,
-                      children: intervals.map(
-                        (el) => InkWell(
-                          onTap: () => _onTap(el['key']),
-                          child: Chip(
-                            backgroundColor: _selectedInterval == el['key'] ? Colors.blueGrey : null,
-                            label: Text(el['label'] as String)
+                height: viewHeight,
+                alignment: Alignment.center,
+                child: _loading ? CircularProgressIndicator() : Stack(children: <Widget>[
+                  Container(
+                    height: viewHeight - 50 - 60,
+                    width: double.infinity,
+                    child: KChartWidget(
+                      _kLines,
+                      isLine: false,
+                      mainState: MainState.BOLL,
+                      secondaryState: SecondaryState.WR,
+                      fixedLength: 2,
+                      timeFormat: TimeFormat.YEAR_MONTH_DAY,
+                      maDayList: [5,10,20],
+                      bgColor: [Colors.black, Colors.black],
+                      isChinese: false,
+                      // onLoadMore: (bool a) {},
+                      // isOnDrag: (isDrag){},
+                    ),
+                  ),
+
+                  Positioned(
+                    bottom: 50,
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                      height: 60,
+                      padding: EdgeInsets.only(top: 10),
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          SizedBox(width: 10),
+                          Wrap(
+                            spacing: 10,
+                            children: intervals.map(
+                              (el) => InkWell(
+                                onTap: () => _onTap(el['key']),
+                                child: Chip(
+                                  backgroundColor: _selectedInterval == el['key'] ? Colors.blueGrey : null,
+                                  label: Text(el['label'] as String)
+                                ),
+                              )
+                            ).toList()
                           ),
-                        )
-                      ).toList()
+                          SizedBox(width: 10),
+                        ]
+                      ),
                     ),
-                    SizedBox(width: 10),
-                  ]
-                ),
+                  ),
+                ]),
               ),
-              _loading ? Text('Loading') : Container(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text('Current Price:'),
-                        SizedBox(width: 20,),
-                        Text(currentPrice != null ? '$currentPrice\$' : 'unknown'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text('Regular Market Change:'),
-                        SizedBox(width: 20,),
-                        Text(regularMarketChange != null ? '$regularMarketChange%' : 'unknown'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text('50 Days Average Change:'),
-                        SizedBox(width: 20,),
-                        Text(fiftyDaysAverageChange != null ? '$fiftyDaysAverageChange\$' : 'unknown'),
-                      ],
-                    ),
-                    SizedBox(height: 30,),
-                    Row(
-                      children: _orderbookData != null ? [
-                        // Text('${_orderbookData!['time']}'),
-                        Flexible(
-                          child: Text('${_orderbookData!['payload']}'),
-                        ),
-                      ] : [],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+
+              // Container(
+              //   height: 250,
+              //   alignment: Alignment.center,
+              //   width: double.infinity,
+              //   color: Colors.black,
+              //   child: (
+              //     _loading
+              //       ? CircularProgressIndicator()
+              //       : DepthChart(_bids, _asks)
+              //   ),
+              // ),
+            ]),
+          ],
+        ),
        ),
     );
   }
